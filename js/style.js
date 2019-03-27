d2d8fe43f55ddbc75d9072c9e77df443e6501b0e
Marco Keller <mkeller@fzi.de>
2019-03-27 16:07:12 +0100

    try to add new features

--- js/style.js
+++ js/style.js
@@ -2,10 +2,12 @@ var canvas;
 var ctx;
 var ratio;
 
+// Index am rand
 const RANDINDEX_Y = 40;
 const RANDINDEX_X = 220;
 const DIFF_KREIS_RECHTECK = 20;
 
+//Radius
 const PUNKTRADIUS = 20;
 const KREISRADIUS = 130;
 
@@ -14,17 +16,12 @@ var refresh = 10;
 //ID von setIntervall;
 var refreshID;
 
- //Aktuell ausgelesene Werte der Orientierungssensoren
+//Aktuell ausgelesene Werte der Orientierungssensoren
 var accelx = 0.0;
 var accely = 0.0;
 var accelz = 0.0;
 
-//Beschleunigung im letzten Intervall
-// aktuelle Beschleunigung * letzte < 0 bedeutet Richtungsänderung
-var lastAccx = 0;
-var lastAccy = 0;
-var lastAccz = 0;
-
+//InitialPosition der drei Punkte im mode 0
 var INIT_X_1 = RANDINDEX_X + PUNKTRADIUS;
 var INIT_Y_1 = RANDINDEX_Y + KREISRADIUS;
 var INIT_X_2 = RANDINDEX_X + 2 * PUNKTRADIUS + DIFF_KREIS_RECHTECK + KREISRADIUS;
@@ -32,10 +29,12 @@ var INIT_Y_2 = RANDINDEX_Y + DIFF_KREIS_RECHTECK + PUNKTRADIUS + 2 * KREISRADIUS
 var INIT_X_3 = RANDINDEX_X + 2 * PUNKTRADIUS + DIFF_KREIS_RECHTECK + KREISRADIUS;
 var INIT_Y_3 = RANDINDEX_Y + KREISRADIUS;
 
-var punkt1 = { x: INIT_X_1, y: INIT_Y_1, radius: PUNKTRADIUS };
-var punkt2 = { x: INIT_X_2, y: INIT_Y_2, radius: PUNKTRADIUS };
-var punkt3 = { x: INIT_X_3, y: INIT_Y_3, radius: PUNKTRADIUS };
-var way = 1;
+var points = [];
+points[0] = { x: INIT_X_1, y: INIT_Y_1, radius: PUNKTRADIUS };
+points[1] = { x: INIT_X_2, y: INIT_Y_2, radius: PUNKTRADIUS };
+points[2] = { x: INIT_X_3, y: INIT_Y_3, radius: PUNKTRADIUS };
+// Anzeigemodus (0 = Alles, 1 = Vertical, 2 = Horizontal)
+var mode = 0;
 
 function startWasserwaage() {
 	initCanvas();
@@ -111,6 +110,7 @@ function init(){
             accelz = 0.0;	
 
 			refresh = 75;
+            mode = 0;
 		}
 
 
@@ -134,14 +134,60 @@ function draw(){
 */
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	
-    drawBackground();
+	switch(mode) {
+        case 0:
+            drawBackgroundAll();
+            break;
+        case 1:
+            drawBackgroundVertical();
+            break;
+        case 2:
+            drawBackgroundHorizontal();
+            break;
+    }
     setPointsPosition();
     checkCollision();
     drawPoints();    
 }
 
-function drawBackground() {
+function drawBackgroundVertical() {
+    ctx.fillStyle = 'hsla(120,100%,50%,0.2)';
+    ctx.strokeRect(RANDINDEX_X, RANDINDEX_Y, 2 * PUNKTRADIUS, 2 * KREISRADIUS);
+    ctx.fillRect(RANDINDEX_X, RANDINDEX_Y, 2 * PUNKTRADIUS, 2 * KREISRADIUS);
+    ctx.beginPath();
+        ctx.strokeStyle = 'black';
+        ctx.moveTo(RANDINDEX_X, RANDINDEX_Y + KREISRADIUS - PUNKTRADIUS - 2);
+        ctx.lineTo(RANDINDEX_X + 2 * PUNKTRADIUS, RANDINDEX_Y + KREISRADIUS - PUNKTRADIUS - 2);
+        ctx.stroke();
+    ctx.beginPath();
+        ctx.strokeStyle = 'black';
+        ctx.moveTo(RANDINDEX_X, RANDINDEX_Y + KREISRADIUS + PUNKTRADIUS + 2);
+        ctx.lineTo(RANDINDEX_X + 2 * PUNKTRADIUS, RANDINDEX_Y + KREISRADIUS + PUNKTRADIUS + 2);
+        ctx.stroke();
+}
+
+function drawBackgroundVertical() {
+    ctx.fillStyle = 'hsla(120,100%,50%,0.2)';
+
+    ctx.strokeRect(RANDINDEX_X + 2 * PUNKTRADIUS + DIFF_KREIS_RECHTECK ,RANDINDEX_Y + 2 * KREISRADIUS + DIFF_KREIS_RECHTECK,
+                 2 * KREISRADIUS, 2 * PUNKTRADIUS);
+    ctx.fillRect(RANDINDEX_X + 2 * PUNKTRADIUS + DIFF_KREIS_RECHTECK ,RANDINDEX_Y + 2 * KREISRADIUS + DIFF_KREIS_RECHTECK,
+                 2 * KREISRADIUS, 2 * PUNKTRADIUS);
+    ctx.beginPath();
+        ctx.strokeStyle = 'black';
+        ctx.moveTo(RANDINDEX_X + PUNKTRADIUS + KREISRADIUS + DIFF_KREIS_RECHTECK - 2, RANDINDEX_Y + 2 * KREISRADIUS + DIFF_KREIS_RECHTECK);
+        ctx.lineTo(RANDINDEX_X + PUNKTRADIUS + KREISRADIUS + DIFF_KREIS_RECHTECK - 2, 
+                    RANDINDEX_Y + 2 * KREISRADIUS + DIFF_KREIS_RECHTECK + 2 * PUNKTRADIUS);
+        ctx.stroke();
+    ctx.beginPath();
+        ctx.strokeStyle = 'black';
+        ctx.moveTo(RANDINDEX_X + 3 * PUNKTRADIUS + KREISRADIUS + DIFF_KREIS_RECHTECK + 2, RANDINDEX_Y + 2 * KREISRADIUS + DIFF_KREIS_RECHTECK);
+        ctx.lineTo(RANDINDEX_X + 3 * PUNKTRADIUS + KREISRADIUS + DIFF_KREIS_RECHTECK + 2, 
+                    RANDINDEX_Y + 2 * KREISRADIUS + DIFF_KREIS_RECHTECK + 2 * PUNKTRADIUS);
+        ctx.stroke();
+}
+
+function drawBackgroundAll() {
 	// Kreis
 	ctx.beginPath();
         ctx.arc(RANDINDEX_X + 2 * PUNKTRADIUS + DIFF_KREIS_RECHTECK + KREISRADIUS,
@@ -160,6 +206,7 @@ function drawBackground() {
 
     // vertikales Rechteck
     ctx.fillStyle = 'hsla(120,100%,50%,0.2)';
+    ctx.strokeRect(RANDINDEX_X, RANDINDEX_Y, 2 * PUNKTRADIUS, 2 * KREISRADIUS);
     ctx.fillRect(RANDINDEX_X, RANDINDEX_Y, 2 * PUNKTRADIUS, 2 * KREISRADIUS);
     ctx.beginPath();
         ctx.strokeStyle = 'black';
@@ -174,8 +221,11 @@ function drawBackground() {
 
     //Horizontales Rechteck
     ctx.fillStyle = 'hsla(120,100%,50%,0.2)';
-    ctx.fillRect(RANDINDEX_X + 2 * PUNKTRADIUS + DIFF_KREIS_RECHTECK ,RANDINDEX_Y + 2 * KREISRADIUS + DIFF_KREIS_RECHTECK,
+
+    ctx.strokeRect(RANDINDEX_X + 2 * PUNKTRADIUS + DIFF_KREIS_RECHTECK ,RANDINDEX_Y + 2 * KREISRADIUS + DIFF_KREIS_RECHTECK,
     			 2 * KREISRADIUS, 2 * PUNKTRADIUS);
+    ctx.fillRect(RANDINDEX_X + 2 * PUNKTRADIUS + DIFF_KREIS_RECHTECK ,RANDINDEX_Y + 2 * KREISRADIUS + DIFF_KREIS_RECHTECK,
+                 2 * KREISRADIUS, 2 * PUNKTRADIUS);
     ctx.beginPath();
         ctx.strokeStyle = 'black';
         ctx.moveTo(RANDINDEX_X + PUNKTRADIUS + KREISRADIUS + DIFF_KREIS_RECHTECK - 2, RANDINDEX_Y + 2 * KREISRADIUS + DIFF_KREIS_RECHTECK);
@@ -191,33 +241,33 @@ function drawBackground() {
 }
 
 function setPointsPosition() {
-    punkt1.y = INIT_Y_1 + (KREISRADIUS - PUNKTRADIUS) * accely;
+    points[0].y = INIT_Y_1 + (KREISRADIUS - PUNKTRADIUS) * accely;
 
-    punkt2.x = INIT_X_2 + (KREISRADIUS - PUNKTRADIUS) * accelx;
+    points[1].x = INIT_X_2 + (KREISRADIUS - PUNKTRADIUS) * accelx;
 
-    punkt3.y = INIT_Y_3 + (KREISRADIUS - PUNKTRADIUS) * accely;
-    punkt3.x = INIT_X_3 + (KREISRADIUS - PUNKTRADIUS) * accelx;
+    points[2].y = INIT_Y_3 + (KREISRADIUS - PUNKTRADIUS) * accely;
+    points[2].x = INIT_X_3 + (KREISRADIUS - PUNKTRADIUS) * accelx;
 }
 
 function checkCollision() {
-    if(punkt1.y <= RANDINDEX_Y + PUNKTRADIUS) {
-        punkt1.y = RANDINDEX_Y + PUNKTRADIUS;
+    if(points[0].y <= RANDINDEX_Y + PUNKTRADIUS) {
+        points[0].y = RANDINDEX_Y + PUNKTRADIUS;
     }
-    if(punkt1.y >= RANDINDEX_Y + 2 * KREISRADIUS - PUNKTRADIUS) {
-        punkt1.y = RANDINDEX_Y + 2 * KREISRADIUS - PUNKTRADIUS;
+    if(points[0].y >= RANDINDEX_Y + 2 * KREISRADIUS - PUNKTRADIUS) {
+        points[0].y = RANDINDEX_Y + 2 * KREISRADIUS - PUNKTRADIUS;
     }
-    if(punkt2.x <= RANDINDEX_X + 3 * PUNKTRADIUS + DIFF_KREIS_RECHTECK) {
-        punkt2.x = RANDINDEX_X + 3 * PUNKTRADIUS + DIFF_KREIS_RECHTECK;
+    if(points[1].x <= RANDINDEX_X + 3 * PUNKTRADIUS + DIFF_KREIS_RECHTECK) {
+        points[1].x = RANDINDEX_X + 3 * PUNKTRADIUS + DIFF_KREIS_RECHTECK;
     }
-    if(punkt2.x >= RANDINDEX_X + PUNKTRADIUS + DIFF_KREIS_RECHTECK + 2 * KREISRADIUS) {
-        punkt2.x = RANDINDEX_X + PUNKTRADIUS + DIFF_KREIS_RECHTECK + 2 * KREISRADIUS;
+    if(points[1].x >= RANDINDEX_X + PUNKTRADIUS + DIFF_KREIS_RECHTECK + 2 * KREISRADIUS) {
+        points[1].x = RANDINDEX_X + PUNKTRADIUS + DIFF_KREIS_RECHTECK + 2 * KREISRADIUS;
     }
 
-    if(getDistanceBetweenPositions(INIT_X_3, INIT_Y_3, punkt3.x, punkt3.y) > (KREISRADIUS - PUNKTRADIUS)) {
-        var tempx = getCircleValueX(INIT_X_3, (punkt3.y - INIT_Y_3), (punkt3.x - INIT_X_3), (KREISRADIUS - PUNKTRADIUS));
-        var tempy = getCircleValueY(INIT_Y_3, (punkt3.y - INIT_Y_3), (punkt3.x - INIT_X_3), (KREISRADIUS - PUNKTRADIUS));
-        punkt3.x = tempx;
-        punkt3.y = tempy;
+    if(getDistanceBetweenPositions(INIT_X_3, INIT_Y_3, points[2].x, points[2].y) > (KREISRADIUS - PUNKTRADIUS)) {
+        var tempx = getCircleValueX(INIT_X_3, (points[2].y - INIT_Y_3), (points[2].x - INIT_X_3), (KREISRADIUS - PUNKTRADIUS));
+        var tempy = getCircleValueY(INIT_Y_3, (points[2].y - INIT_Y_3), (points[2].x - INIT_X_3), (KREISRADIUS - PUNKTRADIUS));
+        points[2].x = tempx;
+        points[2].y = tempy;
     }
 
 }
@@ -248,24 +298,20 @@ function getDistanceBetweenPositions(StartX, StartY, EndX, EndY) {
 }
 
 function drawPoints() {
-		//punkt1.x = punkt1.x + 3* way; 
-        ctx.beginPath();
-        ctx.arc(punkt1.x, punkt1.y, PUNKTRADIUS, 0, Math.PI * 2);
-        ctx.fillStyle = "#04B45F";
-        ctx.fill();
-        ctx.closePath();
-
-        ctx.beginPath();
-        ctx.arc(punkt2.x, punkt2.y, PUNKTRADIUS, 0, Math.PI * 2);
-        ctx.fillStyle = "#04B45F";
-        ctx.fill();
-        ctx.closePath();
-
-        ctx.beginPath();
-        ctx.arc(punkt3.x, punkt3.y, PUNKTRADIUS, 0, Math.PI * 2);
-        ctx.fillStyle = "#04B45F";
-        ctx.fill();
-        ctx.closePath();
+		var min = 0;
+        var max = 3;
+        switch(mode) {
+            case 0: break;
+            case 1: max = 1; break;
+            case 2: min = 1; max = 2; break;
+        }
+        for(var i = min; i < max; i++) {
+            ctx.beginPath();
+            ctx.arc(points[i].x, points[i].y, PUNKTRADIUS, 0, Math.PI * 2);
+            ctx.fillStyle = "#04B45F";
+            ctx.fill();
+            ctx.closePath();
+        }
 }
 
 //Ermittelt die Beschleunigung entsprechend der Bildschirmorientierung
@@ -273,9 +319,24 @@ function drawPoints() {
             window.addEventListener("devicemotion", function (event) {
                 switch (window.orientation) {
                     case 0:
-                        accelx = event.accelerationIncludingGravity.x * (-1);
-                        accely = event.accelerationIncludingGravity.y;
                         accelz = event.accelerationIncludingGravity.z * (-1);
+                        if(accelz <= 3 && accelz >= -3) {
+                            accelx = event.accelerationIncludingGravity.x * (-1);
+                            accely = event.accelerationIncludingGravity.y;
+                            if(!(accely <= 8 && accely >= -8)) {
+                                mode = 2;
+                                break;
+                            }
+                            if(!(accelx <= 8 && accelx >= -8)) {
+                                mode = 1;
+                                break;
+                            }
+
+                        } else {
+                            mode = 0;
+                            accelx = event.accelerationIncludingGravity.x * (-1);
+                            accely = event.accelerationIncludingGravity.y;
+                        }
                         break;
 
                     case -90:
