9c47569880848d2747c3ffb8833bf402804528d1
Marco Keller <mkeller@fzi.de>
2019-04-04 21:42:13 +0200

    test 2

--- js/style.js
+++ js/style.js
@@ -293,7 +293,7 @@ function setPointsPosition() {
 			points[1].x = INIT_X_2 + 4.5 * PUNKTRADIUS * accelx;
 			break;
 		case 2:
-			points[2].y = INIT_Y_3 + 7 * PUNKTRADIUS * accely;
+			points[2].x = INIT_Y_3 + 7 * PUNKTRADIUS * accelx;
 			break;
 	}
 }
